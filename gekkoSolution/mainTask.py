45f509447024853ca064447faa370ee6d2d14081
xiaomage <mazheng_2008ok@163.com>
2018-12-11 19:03:41 +0800

    循环计算增加值优化为：步长*目标函数量值

--- gekkoSolution/mainTask.py
+++ gekkoSolution/mainTask.py
@@ -8,18 +8,25 @@ from gekkoSolution.gekkoConstruct import ObjectiveConstructBuilder, \
 from gekkoSolution.gekkoProblem import GekkoProblem
 from gekkoSolution.gekkoUtils import cal_weights
 from utils.logger import Logger
+from utils.utils import number_scalar
 
 logger = Logger(__name__, log_file_path='../log/gekko_optimization.log').get()
 
 try:
-    step = 1  # 循环计算步长
-    iters = 10  # 循环次数
+    step = 0.1  # 循环计算步长
+    iters = 5  # 循环次数
 
     start = time.time()
 
     lp = GekkoProblem()
     weights = cal_weights(lp, **{'TFe': 1, 'SiO2': 1, 'COST': 1, 'R': 1, 'SS': 1})
     # weights = cal_weights(lp, **{'COST': 1, 'R': 1})
+
+    cao_index = lp.data.Ingredients_list_name_index.get('CaO'.lower())
+    sio2_index = lp.data.Ingredients_list_name_index.get('SiO2'.lower())
+    r_enable = False
+    if cao_index is not None and sio2_index is not None:
+        r_enable = True
 
     lp.remove_construct("objective")
     objectives = ObjectiveConstructBuilder(lp,
@@ -41,14 +48,17 @@ try:
     objfcnval = None  # 上一次目标函数计算值
     for i in range(iters):
         if objfcnval is not None:  # 防止objfcnval为0，不写if objfcnval
-            lp.prob.Equation(objfcn >= objfcnval + step)
+            lp.prob.Equation(objfcn >= objfcnval + step * number_scalar(objfcnval))
 
         # Solve simulation
         lp.solve(disp=True)
 
         lp.print_solve()
         print(lp.get_price())
-        print(lp.get_ingredient_result())
+        ingredient_result = lp.get_ingredient_result()
+        print(ingredient_result)
+        if r_enable:
+            print("R = ", ingredient_result[cao_index] / ingredient_result[sio2_index])  # 碱度R=CaO/SiO2，目标高碱度
         print("objfcnval ============================================================> ", lp.get_objfcnval())
 
         if objfcnval is None:  # 最优结果
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import math
-import numpy as np
 
 
 def check_nan(num):
@@ -12,30 +11,30 @@ def check_nan_or_zero(num):
     return math.isnan(num) or num == 0
 
 
-if __name__ == '__main__':
-    # a = [1, 2, 3]
-    # c = a.copy()
-    # c.append(6)
-    # print(a)
-    # print(c)
-    # a = (1,2)
-    # x = copy.deepcopy(a)
-    # print(a)
-    # print(x)
-    # print(a == x)
+'''
+    计算一个数的量级，eg：150 --> 100, 0.0551 --> 0.01
+'''
 
-    # lists = [{'x1': 1}, {'x2': 2}, {'x3': 3}]
-    # mm = lists.copy()
-    # mm.append({'x5': 5})
-    # mm[0]['x1']=100
-    # print(lists)
-    # print(mm)
 
-    a = {'x1': 0.01, 'x2': 0.11, 'x3': 11.5, 'x4': 0.06, 'x5': 0.01, 'x6': 0.01, 'x7': 9, 'x8': 40, 'x9': 0.65,
-         'down': 0, 'up': 4}
-    b = {'x1': 0.02, 'x2': 0.08, 'x3': 3, 'x4': 0.9, 'x5': 0, 'x6': 0.01, 'x7': 2.95, 'x8': 8, 'x9': 0.297142857142857,
-         'down': 0, 'up': 1}
-    c = {i: 0 if check_nan_or_zero(a[i]) or check_nan_or_zero(b[i]) else a[i] / b[i] for i in
-         ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9']}
+def number_scalar(num):
+    num = abs(num)
+    if num >= 1:
+        digits = 10 ** int(math.log10(num))
+    else:
+        digits = _float_scalar(num)
+    return digits
 
-    print(c)
+
+'''
+    计算小数的量级，eg：0.0551 --> 0.01
+'''
+
+
+def _float_scalar(num):
+    num = abs(num)
+    n = 0
+    while 1 > num > 0:
+        num *= 10
+        n += 1
+    formats = '%0.' + str(n) + 'f'
+    return float(formats % (0.1 ** n))
