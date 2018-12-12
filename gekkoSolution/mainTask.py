71ce86b924788b74ce4baac09445b97e29605ff2
xiaomage <mazheng_2008ok@163.com>
2018-12-12 14:48:16 +0800

    记录多个步长循环计算结果

--- gekkoSolution/mainTask.py
+++ gekkoSolution/mainTask.py
@@ -19,8 +19,10 @@ try:
     start = time.time()
 
     lp = GekkoProblem()
-    weights = cal_weights(lp, **{'TFe': 1, 'SiO2': 1, 'COST': 1, 'R': 1, 'SS': 1})
-    # weights = cal_weights(lp, **{'COST': 1, 'R': 1})
+    # weights = cal_weights(lp, **{'TFe': 1, 'SiO2': 1, 'COST': 1, 'R': 1, 'SS': 1})
+    weights = cal_weights(lp, **{'COST': 1000, 'R': 1})
+    # weights = cal_weights(lp, **{'R': 1})
+    # weights = cal_weights(lp, **{'COST': 1})
 
     cao_index = lp.data.Ingredients_list_name_index.get('CaO'.lower())
     sio2_index = lp.data.Ingredients_list_name_index.get('SiO2'.lower())
@@ -31,12 +33,12 @@ try:
     lp.remove_construct("objective")
     objectives = ObjectiveConstructBuilder(lp,
                                            (PriceObjectiveConstruct(lp), weights['cost']),
-                                           (IngredientObjectiveConstruct(lp, ingredient_name="TFe", maximum=True),
-                                            weights['tfe']),
-                                           (IngredientObjectiveConstruct(lp, ingredient_name="SiO2", maximum=False),
-                                            weights['sio2']),
+                                           # (IngredientObjectiveConstruct(lp, ingredient_name="TFe", maximum=True),
+                                           #  weights['tfe']),
+                                           # (IngredientObjectiveConstruct(lp, ingredient_name="SiO2", maximum=False),
+                                           #  weights['sio2']),
                                            (RObjectiveConstruct(lp), weights['r']),
-                                           (SSObjectiveConstruct(lp), weights['ss'])
+                                           # (SSObjectiveConstruct(lp), weights['ss'])
                                            )
     lp.add_construct("objective", objectives)
 
@@ -48,7 +50,10 @@ try:
     objfcnval = None  # 上一次目标函数计算值
     for i in range(iters):
         if objfcnval is not None:  # 防止objfcnval为0，不写if objfcnval
-            lp.prob.Equation(objfcn >= objfcnval + step * number_scalar(objfcnval))
+            obj_scalar = number_scalar(objfcnval)
+            lp.prob.Equation(objfcn >= objfcnval + step * obj_scalar)
+            print("objfcn >= {0} + {1} = {2}".format(objfcnval, step * obj_scalar,
+                                                     objfcnval + step * obj_scalar))
 
         # Solve simulation
         lp.solve(disp=True)
