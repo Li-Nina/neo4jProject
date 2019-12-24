0902c0e12d5eb520b817c11e00218bfbe14587d1
lazycat <lkj2015rse@163.com>
2019-12-24 10:54:14 +0800

    1224版

--- serverWeb/apis.py
+++ serverWeb/apis.py
@@ -4,10 +4,12 @@ import csv
 import logging
 # from serverWeb.esRepository import searchExpert
 import os.path
+import uuid
+
 import jieba
 import time
 import os
-from flask import request, Flask, jsonify
+from flask import request, Flask, jsonify, render_template, make_response, send_from_directory
 from jieba.analyse.analyzer import ChineseAnalyzer
 from whoosh.fields import *
 from whoosh.index import create_in
@@ -19,9 +21,10 @@ from serverWeb.esRepository import get_data_by_body, create_indexs_es, index_dat
     import_index_data
 # from serverWeb.repository import getExpertsNodeList
 import json
-from serverWeb.esRepository import es, my_index, my_doc_type, parse_excel,save_to_mysql
+from serverWeb.esRepository import es, my_index, my_doc_type, parse_excel, parse_word,save_to_mysql,unzip_file,untar_file,unrar_file,ungz_file
 from utils.util import fetchEduAndMajor
 from utils.util2 import fetchEduAndMajor_es
+
 
 def after_request(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
@@ -119,80 +122,188 @@ def get_index():
 #     delete_index()
 #     return "success"
 #
-#在csv的基础上直接读mysql
-#
-# -*- coding:UTF-8 -*-
+
+# 将从excel中获取的文件保存到数据库
+
+@app.route('/upload_expert', methods=['POST'])
+def upload_excel():
+    # 获取网页上的文件
+    files = request.files
+    expert_file = files.get('expert_file')
+    if expert_file.filename == '':
+        return "请上传文件", 503
+    file_name = str(time.time()) + '.xlsx'  # 用时间戳当文件名
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    file_path = os.path.join(BASE_DIR, 'data/excel/' + file_name)
+    expert_file.save(file_path)
+    # 解析Excel内容
+    doc = parse_excel(file_path)
+    # 保存到数据库 优化 1.如果expert_id已经存在则更新数据 2.如果expert_id不存在则新增数据
+    result = save_to_mysql(doc)
+    if result == 'ERROR':
+        return '上传文件，并保存到数据库失败！'
+
+    # 重命名文件，以后好找
+    new_file_name = str(doc['Expert_ID']) + '.xlsx'
+    new_file_path = os.path.join(BASE_DIR, 'data/excel/' + new_file_name)
+    # 判断对应名字的文件是否存在，如果已存在，删除原来的文件保存新的文件
+    if os.path.exists(new_file_path):
+        os.remove(new_file_path)
+
+    os.rename(file_path, new_file_path)
+    return '上传文件，并将内容保存到数据库成功！'
+
+@app.route('/upload_expert_word', methods=['POST'])
+def upload_word():
+    # 获取网页上的文件
+    files = request.files
+    expert_file = files.get('expert_file_word')
+    if expert_file.filename == '':
+        return "请上传文件", 503
+    file_name = str(time.time()) + '.docx'  # 用时间戳当文件名
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print(BASE_DIR)
+    file_path = os.path.join(BASE_DIR, 'data\\word\\' + file_name)
+    expert_file.save(file_path)
+    # 解析Excel内容
+    docx = parse_word(file_path)
+    # 保存到数据库 优化 1.如果expert_id已经存在则更新数据 2.如果expert_id不存在则新增数据
+    result = save_to_mysql(docx)
+    if result == 'ERROR':
+        return '上传文件，并保存到数据库失败！'
+
+    # 重命名文件，以后好找
+    new_file_name = str(docx['Expert_ID']).replace('\t', '') + '.docx'
+    new_file_path = os.path.join(BASE_DIR, 'data\\word\\' + new_file_name)
+    # 判断对应名字的文件是否存在，如果已存在，删除原来的文件保存新的文件
+    if os.path.exists(new_file_path):
+        os.remove(new_file_path)
+
+    os.rename(file_path, new_file_path)
+    return '上传文件，并将内容保存到数据库成功！'
+
+
+@app.route("/upload_package", methods=["POST"])
+def upload_package():
+    # 获取网页上的文件
+    files = request.files
+    package_file_obj = files.get('package_file')
+    if package_file_obj.filename == '':
+        return "上传文件为空", 503
+    # 判断压缩包类型 zip、tar、rar、gz、tgz —— 略，都按zip处理
+    ret_list = package_file_obj.filename.rsplit(".", maxsplit=1)
+    if len(ret_list) != 2:
+        return "请上传正确类型压缩包文件", 503
+
+    # 方式：先保存压缩文件到本地，再对其进行解压，然后删除压缩文件
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # 上传的文件保存到的路径
+    file_path = os.path.join(BASE_DIR, "data", package_file_obj.filename)
+    package_file_obj.save(file_path)
+    # 解压后的文件保存到的路径
+    target_path = os.path.join(BASE_DIR, "data", str(uuid.uuid4()))
+
+    if ret_list[1] == "zip":
+        result = unzip_file(file_path, target_path)
+    elif ret_list[1] == "tar":
+        result = untar_file(file_path, target_path)
+    elif ret_list[1] == "rar":
+        result = unrar_file(file_path, target_path)
+    elif ret_list[1] == "gz":
+        result = ungz_file(file_path, target_path)
+    else:
+        return "请上传正确类型的压缩文件", 503
+
+    # 删除原来的压缩包文件
+    os.remove(file_path)
+
+    if result != 'SUCCESS':
+        return result
+    return "上传压缩包成功"
+
+
+@app.route("/download_expert/<filename>", methods=['GET'])
+def download_excel(filename):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    if not os.path.exists(os.path.join(BASE_DIR, 'data/excel/' + filename)):
+        return "下载文件不存在", 503
+    # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)
+    response = make_response(send_from_directory(BASE_DIR + '/data/excel', filename, as_attachment=True))
+    return response
+
+# 在csv的基础上直接读mysql
 # 当数据库新增数据的时候，同时把数据添加到es中 TODO： 需要确定发过来的是[{}, {}]还是{}
-@app.route('/create_es', methods=['GET', 'POST', 'PUT'])
-def create_es():
-    # 接收参数
-    data = request.data
-    data_dict = json.loads(data)
-    doc = dict()
-    doc['Expert_ID'] = data_dict.get('Expert_ID')
-    # doc['fieldID'] = data_dict.get('fieldID')
-    doc['name'] = data_dict.get('name')
-    doc['college'] = data_dict.get('college')
-    doc['education'] = data_dict.get('education')
-    doc['degree'] = data_dict.get('degree')
-    doc['job'] = data_dict.get('job')
-    # doc['professional'] = data_dict.get('professional')
-    doc['subject'] = data_dict.get('subject')
-    doc['phone'] = data_dict.get('phone')
-    doc['address'] = data_dict.get('address')
-    doc['research'] = data_dict.get('research')
-    doc['department'] = data_dict.get('department')
-    doc['title'] = data_dict.get('title')
-    doc['email'] = data_dict.get('email')
-    doc['field'] = data_dict.get('field')
-    doc['project'] = data_dict.get('project')
-    # 校验参数--略
-    # 数据处理
-    res = es.index(index=my_index, doc_type=my_doc_type, body=doc, id=doc['Expert_ID'])
-    # 返回相应
-    return res['result']
+# @app.route('/create_es', methods=['GET', 'POST', 'PUT'])
+# def create_es():
+#     # 接收参数
+#     data = request.data
+#     data_dict = json.loads(data)
+#     doc = dict()
+#     doc['Expert_ID'] = data_dict.get('Expert_ID')
+#     # doc['fieldID'] = data_dict.get('fieldID')
+#     doc['name'] = data_dict.get('name')
+#     doc['college'] = data_dict.get('college')
+#     doc['education'] = data_dict.get('education')
+#     doc['degree'] = data_dict.get('degree')
+#     doc['job'] = data_dict.get('job')
+#     # doc['professional'] = data_dict.get('professional')
+#     doc['subject'] = data_dict.get('subject')
+#     doc['phone'] = data_dict.get('phone')
+#     doc['address'] = data_dict.get('address')
+#     doc['research'] = data_dict.get('research')
+#     doc['department'] = data_dict.get('department')
+#     doc['title'] = data_dict.get('title')
+#     doc['email'] = data_dict.get('email')
+#     doc['field'] = data_dict.get('field')
+#     doc['project'] = data_dict.get('project')
+#     # 校验参数--略
+#     # 数据处理
+#     res = es.index(index=my_index, doc_type=my_doc_type, body=doc, id=doc['Expert_ID'])
+#     # 返回相应
+#     return res['result']
+#
+#
+# # 当数据库“修改”数据的时候，同时修改es中的数据
+# @app.route('/update_es', methods=['POST', 'PATCH'])
+# def update_es():
+#     # 接收参数 TODO: 需要知道对方发来的数据是仅仅变更的字段，还是所有字段
+#     data = request.data
+#     data_dict = json.loads(data)
+#     doc = {}
+#     doc['Expert_ID'] = data_dict.get('Expert_ID')
+#     # doc['fieldID'] = data_dict.get('fieldID')
+#     doc['name'] = data_dict.get('name')
+#     doc['college'] = data_dict.get('college')
+#     doc['education'] = data_dict.get('education')
+#     doc['degree'] = data_dict.get('degree')
+#     doc['job'] = data_dict.get('job')
+#     # doc['professional'] = data_dict.get('professional')
+#     doc['subject'] = data_dict.get('subject')
+#     doc['phone'] = data_dict.get('phone')
+#     doc['address'] = data_dict.get('address')
+#     doc['research'] = data_dict.get('research')
+#     doc['department'] = data_dict.get('department')
+#     doc['title'] = data_dict.get('title')
+#     doc['email'] = data_dict.get('email')
+#     doc['field'] = data_dict.get('field')
+#     doc['project'] = data_dict.get('project')
+#     # 校验参数--略
+#     # 数据处理
+#     # 先删除原来的
+#     es.delete(index=my_index, doc_type=my_doc_type, id=doc['Expert_ID'])
+#     # 再创建新的
+#     res = es.index(index=my_index, doc_type=my_doc_type, body=doc, id=doc['Expert_ID'])
+#     return res['result']
+#
+#
+# # 当数据库“删除”数据的时候，同时把删除es中的数据
+# @app.route('/delete_es', methods=['GET', 'POST', 'DELETE'])
+# def delete_es():
+#     args = request.args
+#     id = args.get('Expert_ID')
+#     result = es.delete(index=my_index, doc_type=my_doc_type, id=id)
+#     return result['result']
 
-
-# 当数据库“修改”数据的时候，同时修改es中的数据
-@app.route('/update_es', methods=['POST', 'PATCH'])
-def update_es():
-    # 接收参数 TODO: 需要知道对方发来的数据是仅仅变更的字段，还是所有字段
-    data = request.data
-    data_dict = json.loads(data)
-    doc = {}
-    doc['Expert_ID'] = data_dict.get('Expert_ID')
-    # doc['fieldID'] = data_dict.get('fieldID')
-    doc['name'] = data_dict.get('name')
-    doc['college'] = data_dict.get('college')
-    doc['education'] = data_dict.get('education')
-    doc['degree'] = data_dict.get('degree')
-    doc['job'] = data_dict.get('job')
-    # doc['professional'] = data_dict.get('professional')
-    doc['subject'] = data_dict.get('subject')
-    doc['phone'] = data_dict.get('phone')
-    doc['address'] = data_dict.get('address')
-    doc['research'] = data_dict.get('research')
-    doc['department'] = data_dict.get('department')
-    doc['title'] = data_dict.get('title')
-    doc['email'] = data_dict.get('email')
-    doc['field'] = data_dict.get('field')
-    doc['project'] = data_dict.get('project')
-    # 校验参数--略
-    # 数据处理
-    # 先删除原来的
-    es.delete(index=my_index, doc_type=my_doc_type, id=doc['Expert_ID'])
-    # 再创建新的
-    res = es.index(index=my_index, doc_type=my_doc_type, body=doc, id=doc['Expert_ID'])
-    return res['result']
-
-
-# 当数据库“删除”数据的时候，同时把删除es中的数据
-@app.route('/delete_es', methods=['GET', 'POST', 'DELETE'])
-def delete_es():
-    args = request.args
-    id = args.get('Expert_ID')
-    result = es.delete(index=my_index, doc_type=my_doc_type, id=id)
-    return result['result']
 
 @app.route("/es_my_create_index", methods=['GET', 'POST'])
 def es_my_create_index():
@@ -204,32 +315,6 @@ def es_my_create_index():
     # TODO: FieldID的索引时long类型，不能是字符串
     import_index_data(mysql_data)
     return "success"
-#将从excel中获取的文件保存到数据库和ES中
-@app.route('/upload_expert', methods=['POST'])
-def upload_excel():
-    # 获取网页上的文件
-    files = request.files
-    expert_file = files.get('expert_file')
-    if expert_file is None:
-        return "文件读取失败", 503
-    file_name = str(time.time()) + '.xlsx'  # 用时间戳当文件名
-    file_path = os.path.join(app.static_folder + '/', file_name)
-    # with open(file_path, 'wb') as f:
-    #     f.write(expert_file.read())
-    expert_file.save(file_path)
-    print('为了保存文件，睡10秒')  # TODO：需要优化
-    time.sleep(10)
-    # 解析Excel内容
-    doc = parse_excel(file_path)
-    # 保存到ES中
-    es.index(index=my_index, doc_type=my_doc_type, body=doc, id=doc['Expert_ID'])
-    # 保存到数据库
-    save_to_mysql(doc)
-    # 重命名文件，以后好找
-    new_file_name = str(doc['Expert_ID']) + '.xlsx'
-    new_file_path = os.path.join(app.static_folder, new_file_name)
-    os.rename(file_path, new_file_path)
-    return '上传文件，并将内容保存到数据库和ES成功！'
 
 
 @app.route("/es_my_get_data", methods=['GET', 'POST'])
@@ -249,3 +334,81 @@ def es_my_get_data():
     # ###############################################
     return jsonify({'json_hit': json_hit, 'segments': segments})
     ###############################################
+##################################################################
+# 展示所有数据
+@app.route('/experts')
+def experts():
+    # 接收参数--无
+    # 校验参数--无
+    # 数据处理
+    experts = get_data_from_mysql()
+    # 返回相应
+    return jsonify(experts)
+
+
+# 新增数据
+@app.route("/add_expert", methods=["POST"])
+def add_expert():
+    # 接收参数
+    data = request.data
+    data_dict = json.loads(data)
+
+    # 校验参数
+    if not data_dict["Expert_ID"]:
+        return jsonify({"message": "ERROR"})
+    # 在ES中并没有这一项，但是数据库的插入语句中有
+    if "professional" not in data_dict.keys():
+        data_dict.update({"professional": ""})
+    # 数据处理——增加到数据库
+    result = save_to_mysql(data_dict)
+    if result != "SUCCESS":
+        return jsonify({"message": "ERROR"})
+    # 新增数据的同时，将数据添加到ES中
+    res = es.index(index=my_index, doc_type=my_doc_type, body=data_dict, id=data_dict['Expert_ID'])
+    if (res['result'] != "created") and (res['result'] != "updated"):
+        print("!!!!!!!数据库新增数据后，ES中添加失败！")
+    # 返回响应
+    return jsonify({"message": "SUCCESS"})
+
+
+# 修改数据
+@app.route("/update_expert", methods=["POST"])
+def update_expert():
+    # 接收参数
+    data = request.data
+    data_dict = json.loads(data)
+
+    # 校验参数
+    if not data_dict["Expert_ID"]:
+        return jsonify({"message": "ERROR"})
+    # 在ES中并没有这一项，但是数据库的插入语句中有
+    if "professional" not in data_dict.keys():
+        data_dict.update({"professional": ""})
+    # 数据处理——增加到数据库
+    result = save_to_mysql(data_dict)
+    if result != "SUCCESS":
+        return jsonify({"message": "ERROR"})
+    # 先删除原来的
+    es.delete(index=my_index, doc_type=my_doc_type, id=data_dict['Expert_ID'])
+    # 再创建新的
+    res = es.index(index=my_index, doc_type=my_doc_type, body=data_dict, id=data_dict['Expert_ID'])
+    if (res['result'] != "created") and (res['result'] != "updated"):
+        print("!!!!!!!数据库更新数据后，ES中更新失败！")
+    # 返回响应
+    return jsonify({"message": "SUCCESS"})
+
+
+# 删除数据
+@app.route("/delete_expert")
+def delete_expert():
+    args = request.args
+    id = args.get('Expert_ID')
+    # 删除数据库中的数据
+    result = save_to_mysql({"Expert_ID": id})
+    if result != "SUCCESS":
+        return jsonify({"message": "ERROR"})
+    result = es.delete(index=my_index, doc_type=my_doc_type, id=id)
+    if result['result'] != "deleted":
+        print("!!!!!!!数据库更新数据后，ES中更新失败！")
+    # 返回响应
+    return jsonify({"message": "SUCCESS"})
--- serverWeb/esRepository.py
+++ serverWeb/esRepository.py
@@ -1,5 +1,10 @@
+import gzip
 import logging
 import csv
+import tarfile
+import zipfile
+import rarfile
+
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from elasticsearch_dsl import connections
@@ -7,6 +12,7 @@ from config import CLUSTER_NODES
 import pymysql
 import openpyxl
 import os
+import docx
 import time
 
 
@@ -254,7 +260,9 @@ def get_data_from_mysql():
 def import_index_data(index_data):
     for data in index_data:
         es.index(index=my_index, doc_type=my_doc_type, body=data, id=data['Expert_ID'])
-#解析excel文件
+
+
+# 解析Excel文件
 def parse_excel(excel_path):
     """
     excel_path: excel文件路径
@@ -262,9 +270,10 @@ def parse_excel(excel_path):
     """
     my_book = openpyxl.load_workbook(excel_path)
     my_sheet = my_book['Sheet1']
+
     # 开始解析数据
     doc = {}
-    doc['Expert_ID'] = my_sheet.cell(2, 6).value
+    doc['Expert_ID'] = str(my_sheet.cell(2, 6).value)
     doc['fieldID'] = my_sheet.cell(2, 8).value
     doc['name'] = my_sheet.cell(3, 2).value
     doc['college'] = my_sheet.cell(7, 2).value
@@ -281,6 +290,38 @@ def parse_excel(excel_path):
     doc['email'] = my_sheet.cell(6, 6).value
     doc['field'] = my_sheet.cell(5, 8).value
     doc['project'] = my_sheet.cell(11, 2).value
+    return doc
+
+# 解析word文件
+word_path = ''
+def parse_word(word_path):
+    """
+    excel_path: excel文件路径
+    return: 解析后的字典
+    """
+    file = docx.Document(word_path)
+    t = file.tables[0]
+    # 开始解析数据
+    doc = {}
+    expert_id = file.paragraphs[1].text
+    doc['Expert_ID'] = expert_id.split(':')[1]
+    field_id = file.paragraphs[2].text
+    doc['fieldID'] =  field_id.split(':')[1]
+    doc['name'] = t.cell(0, 1).text
+    doc['title'] = t.cell(0, 7).text
+    doc['education'] = t.cell(1, 1).text
+    doc['degree'] = t.cell(1, 3).text
+    doc['job'] = t.cell(1, 5).text
+    doc['professional'] = t.cell(1, 7).text
+    doc['subject'] = t.cell(2, 1).text
+    doc['college'] = t.cell(2, 3).text
+    doc['email'] = t.cell(2, 5).text
+    doc['field'] = t.cell(3, 1).text
+    doc['department'] = t.cell(3, 3).text
+    doc['phone'] = t.cell(3, 5).text
+    doc['address'] = t.cell(4, 1).text
+    doc['research'] = t.cell(5, 1).text
+    doc['project'] = t.cell(6, 1).text
     return doc
 
 # 保存到数据库
@@ -301,25 +342,146 @@ def save_to_mysql(data):
         return "数据保存失败"
 
     try:
-        insert_sql = "insert into texpert_info(expert_id, fieldId, name, college, education, degree, " \
-                     "job, professional, subject, phonenumber, address, research, department, " \
-                     "title, email, field, project) " \
-                     "value(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s);"
-        # 这里字典的顺序和SQL语句参数的顺序一致，所以可以直接.values()
-        rows = cursor.execute(insert_sql, args=list(data.values()))
-        if rows == 0:
-            return "数据保存失败，可能是SQL语句的问题"
+        if len(data.keys()) == 1 and "Expert_ID" in data.keys():
+            # 仅仅有一个key，还是Expert_ID说明这是为了执行删除操作
+            delete_sql = "delete from texpert_info where expert_id=%s;"
+            delete_rows = cursor.execute(delete_sql, args=[data['Expert_ID']])
+            if delete_rows == 0:
+                raise pymysql.MySQLError('数据删除失败，可能是SQL语句或数据类型的问题')
+            else:
+                return "SUCCESS"
+        # 先查Expert_id对应的数据是否存在
+        select_sql = "select expert_id from texpert_info where expert_id=%s;"
+        select_rows = cursor.execute(select_sql, args=[data['Expert_ID']])
+        if select_rows == 0:
+            print('Expert_id不存在，创建新记录')
+            insert_sql = "insert into texpert_info(id, expert_id, fieldId, name, college, education, degree, job, " \
+                         "professional, subject, phonenumber, address, research, department, title, email, field, " \
+                         "project) value(%(Expert_ID)s, %(Expert_ID)s, %(fieldID)s, %(name)s, %(college)s, " \
+                         "%(education)s, %(degree)s, %(job)s, %(professional)s, %(subject)s, %(phone)s, %(address)s," \
+                         "%(research)s, %(department)s, %(title)s, %(email)s, %(field)s, %(project)s);"
+            # 这里字典的顺序和SQL语句参数的顺序一致，所以可以直接.values()
+            print(cursor.mogrify(insert_sql, args=data))
+            rows = cursor.execute(insert_sql, args=data)
+            if rows == 0:
+                raise pymysql.MySQLError('数据插入失败，可能是SQL语句或数据类型的问题')
+        else:
+            print('Expert_id存在，更新记录')
+            update_sql = "update texpert_info set fieldId=%(fieldID)s, name=%(name)s, college=%(college)s, " \
+                         "education=%(education)s, degree=%(degree)s, job=%(job)s, professional=%(professional)s, " \
+                         "subject=%(subject)s, phonenumber=%(phone)s, address=%(address)s, research=%(research)s, " \
+                         "department=%(department)s, title=%(title)s, email=%(email)s, field=%(field)s, " \
+                         "project=%(project)s where expert_id=%(Expert_ID)s;"
+            # 查看生成的SQL语句
+            print(cursor.mogrify(update_sql, args=data))
+            rows = cursor.execute(update_sql, args=data)
+            print('修改影响的行数：', rows)
         conn.commit()
-        return "数据保存成功"
+        return "SUCCESS"
 
-    except pymysql.MySQLError as e:
+    except Exception as e:
         print('数据库操作异常，开始回滚', e)
         conn.rollback()
-        return "数据保存失败"
+        return "ERROR"
     finally:
         # 5：关闭游标、关闭连接
         cursor.close()
         conn.close()
+
+
+# 解压压缩包
+def unzip_file(zip_src, dst_dir):
+    """
+    1.解压zip文件
+    2.如果是图片保存到data/pic目录下；
+    3.如果是Excel，解析数据并将数据保存到数据库，然后文件放到data目录下
+    :param zip_src: zip文件的全路径
+    :param dst_dir: 要解压到的目的文件夹
+    :param filename: 压缩包名字
+    :return:
+    """
+    # 判断是否是压缩文件
+    if not zipfile.is_zipfile(zip_src):
+        return "请上传正确的zip压缩文件"
+    fz = zipfile.ZipFile(zip_src, "r")
+    fz.extractall(dst_dir)
+    fz.close()
+
+    # 查找解压后的文件，Excel就解析数据保存到数据库
+    find_excel_file(dst_dir)
+    return "SUCCESS"
+
+# 解压rar类型压缩包(经测试，rar文件不可用)
+def unrar_file(rar_src, dst_dir):
+    # 判断是否是压缩文件
+    if not rarfile.is_rarfile(rar_src):
+        return "请上传正确的rar压缩文件"
+    rar = rarfile.RarFile(rar_src, "r")
+    rar.extractall(dst_dir)
+    rar.close()
+
+    # 查找解压后的文件，Excel就解析数据保存到数据库
+    find_excel_file(dst_dir)
+    return "SUCCESS"
+
+# 解压gz类型压缩包
+def ungz_file(rar_src, dst_dir):
+    gz = gzip.GzipFile(rar_src, "r")
+    open(dst_dir, "w+").write(gz.read())
+    gz.close()
+
+    # 查找解压后的文件，Excel就解析数据保存到数据库
+    find_excel_file(dst_dir)
+    return "SUCCESS"
+
+# 解压tar类型压缩包
+def untar_file(tar_src, dst_dir):
+    # 判断是否是压缩文件
+    if not tarfile.is_tarfile(tar_src):
+        return "请上传正确的tar压缩文件"
+    tar = tarfile.open(tar_src, "r")
+    tar.extractall(dst_dir)
+    tar.close()
+
+    # 查找解压后的文件，Excel就解析数据保存到数据库
+    find_excel_file(dst_dir)
+    return "SUCCESS"
+
+def find_excel_file(path):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    files = os.listdir(path)
+    for file in files:
+        this_path = os.path.join(path, file)
+        if os.path.isdir(this_path):
+            find_excel_file(this_path)
+        else:
+            ret_list = file.rsplit(".", maxsplit=1)
+            if ret_list[1] in ['xlsx', 'xls']:
+                print(f"{file}是Excel文件，开始解析里面的数据。")
+                # 开始读取里面的数据保存到数据库
+                file_path = os.path.join(path, file)
+                # 解析Excel模板中的数据
+                doc = parse_excel(file_path)
+                # 保存到数据库 优化 1.如果expert_id已经存在则更新数据 2.如果expert_id不存在则新增数据
+                result = save_to_mysql(doc)
+                if result == 'ERROR':
+                    return '上传文件，并保存到数据库失败！', 500
+
+                # 重命名文件
+                new_file_name = str(doc['Expert_ID']) + '.xlsx'
+                new_file_path = os.path.join(BASE_DIR, 'data/excel/' + new_file_name)
+                # 判断对应名字的文件是否存在，如果已存在，删除原来的文件保存新的文件
+                if os.path.exists(new_file_path):
+                    os.remove(new_file_path)
+
+                os.renames(file_path, new_file_path)
+            else:
+                # 假设其它都是图片，保存到pictures目录
+                new_file_path = os.path.join(BASE_DIR, 'serverWeb/static/src/pic/' + file)
+                # 判断对应名字的文件是否存在，如果已存在，删除原来的文件保存新的文件
+                if os.path.exists(new_file_path):
+                    os.remove(new_file_path)
+                os.renames(this_path, new_file_path)
 
 # if __name__ == '__main__':
 #     mysql_data = get_data_from_mysql()
